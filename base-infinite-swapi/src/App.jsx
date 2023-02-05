cbe6e47fad2cbf5f034ec14b1d5415c540ae7ead
sosoYim <sohyung.yim@gmail.com>
2023-02-05 09:05:12 +0900

    feat: base-infinite-swapi
    - InfiniteScroll을 이용한 무한 스크롤

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        {/* <InfinitePeople /> */}
+        <InfiniteSpecies />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,38 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["species"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error... {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetchingNextPage && <div>데이터 더 불러오는 중...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map(({ results }, pageIdx) =>
+          results.map(({ name, language, average_lifespan }, idx) => (
+            <Species
+              key={`specie-${pageIdx}-${idx}`}
+              name={name}
+              language={language}
+              averageLifespan={average_lifespan}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
+  );
 }
